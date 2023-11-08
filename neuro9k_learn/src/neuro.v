898d643c2ee87679ba881254232d60db9aaaed9c
michihiro imaoka <imaoca@gmail.com>
2023-11-08 16:32:13 +0900

    Update neuro.v
    Random input characters are given,

--- neuro9k_learn/src/neuro.v
+++ neuro9k_learn/src/neuro.v
@@ -1,3 +1,4 @@
+// Random input characters are given,
 module neuro (input rst,input clk,input [3:0]btn,output test1,test2,low,high,[7:0]col,[7:0]row);
   reg [24:0] neuros;
   wire [7:0] matrix[7:0];
@@ -21,6 +22,33 @@ module neuro (input rst,input clk,input [3:0]btn,output test1,test2,low,high,[7:
   always @(posedge(clk))count12 = count12 + 1;
 
   integer k,m;
+  always @(posedge(clk))begin
+	if (rst==0)  neuros = count12;
+	if (btn[0]==0)if (count12[22]==0) begin
+        for (k=0;k<25;k=k+1) begin 
+            sum=0;
+            for (m=0;m<25;m=m+1) 
+                sum = sum + ((neuros[m]==1)?links[(k<<4)+(k<<3)+k+m]:(-links[(k<<4)+(k<<3)+k+m]));
+        neuros[k]=(sum>0)?1:0;
+        end
+	end
+ end
+
+/*
+  always @(posedge(clk))begin
+	if (rst==0)  neuros = count12;
+	if ((btn[0]==0)&(count12[22]==0))begin
+        for (k=0;k<25;k=k+1) begin 
+            sum=0;
+            for (m=0;m<25;m=m+1) 
+                sum = sum + ((neuros[m]==1)?links[(k<<4)+(k<<3)+k+m]:(-links[(k<<4)+(k<<3)+k+m]));
+        neuros[k]=(sum>0)?1:0;
+        end
+	end
+ end
+*/
+
+/*
   always @(posedge(count12[22]))begin
 	if (rst==0)  begin neuros = 25'b0111010011100100001001110;end
 	if ((btn[0]==0))begin
@@ -32,6 +60,7 @@ module neuro (input rst,input clk,input [3:0]btn,output test1,test2,low,high,[7:
         end
 	end
  end
+*/
 
 /*
   always @(posedge(clk))begin
