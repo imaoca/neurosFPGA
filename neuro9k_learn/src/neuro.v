dd114acf140012af20e49e3f3f6904aea966c8d9
michihiro imaoka <imaoca@gmail.com>
2023-11-08 13:47:53 +0900

    Update neuro.v

--- neuro9k_learn/src/neuro.v
+++ neuro9k_learn/src/neuro.v
@@ -1,13 +1,12 @@
 module neuro (input rst,input clk,input [3:0]btn,output test1,test2,low,high,[7:0]col,[7:0]row);
   reg [24:0] neuros;
-//  reg signed[7:0] links[624:0];
   wire [7:0] matrix[7:0];
   reg[4:0] i=0,jj=0;
   reg signed [7:0]sum;
   reg [24:0]count12;
   reg [24:0] D,J,C,M,noise;
-//  reg [24:0]learn[2:0];
   reg signed[3:0] links[624:0];
+//  reg [24:0]learn[2:0];
 
   assign test1 = i;
   assign test2 = btn[0];
@@ -22,6 +21,19 @@ module neuro (input rst,input clk,input [3:0]btn,output test1,test2,low,high,[7:
   always @(posedge(clk))count12 = count12 + 1;
 
   integer k,m;
+  always @(posedge(count12[22]))begin
+	if (rst==0)  begin neuros = 25'b0111010011100100001001110;end
+	if ((btn[0]==0))begin
+        for (k=0;k<25;k=k+1) begin 
+            sum=0;
+            for (m=0;m<25;m=m+1) 
+                sum = sum + ((neuros[m]==1)?links[(k<<4)+(k<<3)+k+m]:(-links[(k<<4)+(k<<3)+k+m]));
+        neuros[k]=(sum>0)?1:0;
+        end
+	end
+ end
+
+/*
   always @(posedge(clk))begin
 	if (rst==0)  begin i = 0;neuros = 25'b0111010011100100001001110;end
 	if ((btn[0]==0)&&(i==0))begin
@@ -34,6 +46,7 @@ module neuro (input rst,input clk,input [3:0]btn,output test1,test2,low,high,[7:
 		i = 1;
 	end
  end
+*/
 /*
  always @(posedge(clk))if (rst==0) begin 
         learn[0]     = 25'b0111010010100101001001111;
